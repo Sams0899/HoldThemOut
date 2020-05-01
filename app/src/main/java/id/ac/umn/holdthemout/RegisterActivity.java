634543793f1846a7fff0a7bc71107b8fc8c5c5c7
Neven21 <gregoriusneven@yahoo.com>
2020-05-01 11:19:48 +0700

    SQLite Insert di Register

--- app/src/main/java/id/ac/umn/holdthemout/RegisterActivity.java
+++ app/src/main/java/id/ac/umn/holdthemout/RegisterActivity.java
@@ -1,68 +1,56 @@
 package id.ac.umn.holdthemout;
 
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Room;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private UserRoomDatabase userDatabase;
+    SQLiteDatabase sqLiteDatabase;
     private EditText firstname, lastname, username, password;
+    String fname, lname, uname, pass;
     private Button registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        userDatabase = Room.databaseBuilder(getApplicationContext(),
-                UserRoomDatabase.class, "userdatabase").build();
+        sqLiteDatabase=openOrCreateDatabase("htodb", Context.MODE_PRIVATE, null);
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS User(UserID INTEGER PRIMARY KEY AUTOINCREMENT, Username VARCHAR(50), FirstName VARCHAR(50), LastName VARCHAR(50), Password VARCHAR(50), Highscore INTEGER );");
 
-        new DatabaseAsync().execute();
+        firstname = findViewById(R.id.firstname);
+        lastname = findViewById(R.id.lastname);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.Btnregister);
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent registerIntent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(registerIntent);
-                RegisterActivity.this.finish();
+                fname = firstname.getText().toString().trim();
+                lname = lastname.getText().toString().trim();
+                uname = username.getText().toString().trim();
+                pass = password.getText().toString().trim();
+                if(fname.equals("") || lname.equals("") || uname.equals("") || password.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Fields can not be empty", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    sqLiteDatabase.execSQL("Insert Into htodb(Username, FirstName, LastName, Password) VALUES('" + uname + "','" + fname + "','" + lname +"','" + pass + "');");
+                    Toast.makeText(getApplicationContext(), "Record Saved", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-
-    private class DatabaseAsync extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            //Perform pre-adding operation here.
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            User user = new User();
-            user.setUsername("admin");
-            user.setFirstname("Admin");
-            user.setLastname("Admin");
-            user.setPassword("admin");
-            user.setHighscore(1000);
-
-            userDatabase.daoUser().insert(user);
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            //To after addition operation here.
-        }
-    }
 }
+
