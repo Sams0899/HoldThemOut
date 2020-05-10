61c50dafb7bab1246acb0c1e324782ad1f9d1f9d
natayatayo <natayatayo@gmail.com>
2020-05-10 20:19:05 +0700

    about activity updated

--- app/src/main/java/id/ac/umn/holdthemout/About.java
+++ app/src/main/java/id/ac/umn/holdthemout/About.java
@@ -3,49 +3,41 @@ package id.ac.umn.holdthemout;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
-    private ImageButton btnNeven, btnSamuel, btnWisnu, btnNataya;
+    private TextView nama1, nama2, nama3, nama4, heading;
+    private static int splashTimeOut=5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        nama1 = findViewById(R.id.nama1);
+        nama2 = findViewById(R.id.nama2);
+        nama3 = findViewById(R.id.nama3);
+        nama4 = findViewById(R.id.nama4);
+        heading = findViewById(R.id.heading);
 
-        btnNeven = findViewById(R.id.btnNeven);
-        btnSamuel = findViewById(R.id.btnSamuel);
-        btnWisnu = findViewById(R.id.btnWisnu);
-        btnNataya = findViewById(R.id.btnNataya);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(About.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashTimeOut);
 
-        btnNeven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(About.this, Profil_Neven.class);
-                startActivity(startIntent);
-            }
-        });
-        btnSamuel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(About.this, Profil_Samuel.class);
-                startActivity(startIntent);
-            }
-        });
-        btnWisnu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(About.this, Profil_Wisnu.class);
-                startActivity(startIntent);
-            }
-        });
-        btnNataya.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(About.this, Profil_Nataya.class);
-                startActivity(startIntent);
-            }
-        });
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splash);
+        nama1.startAnimation(myanim);
+        nama2.startAnimation(myanim);
+        nama3.startAnimation(myanim);
+        nama4.startAnimation(myanim);
+        heading.startAnimation(myanim);
     }
 }
